029cba2130a50206d602f6488962465b42c78182
Pedro Jorge <pedro.jlsilva@icloud.com>
2020-11-23 16:09:00 -0300

    resolucao da questao 5

---
+++ Q5/Q5.java
@@ -0,0 +1,113 @@
+public class Q5 {
+    public static void main(String[] args) {
+        ColecaoVetor colecao = new ColecaoVetor();
+        Professor prof = new Professor("joanes", "123.456.789-12", "matematica", "estatistica");
+        colecao.inserir(prof);
+        System.out.println("ainda existem "+colecao.tamanhoColecaoNaoNull()+" vagas.");
+        prof = new Professor("castor", "987.654.321-12", "informatica", "plc");
+        colecao.inserir(prof);
+        System.out.println("ainda existem "+colecao.tamanhoColecaoNaoNull()+" vagas.");
+        Aluno aluno = new Aluno("eu", "963.852.741-12", "eng. comp.", "robotica");
+        colecao.inserir(aluno);
+        System.out.println("ainda existem "+colecao.tamanhoColecaoNaoNull()+" vagas.");
+        
+    }
+}
+
+interface Colecao {
+    public void inserir(Pessoa2 p);
+    public int tamanhoColecaoNaoNull();
+}
+
+class ColecaoVetor implements Colecao{
+    private int cursor;
+    private Pessoa2[] pessoas ;
+
+    @Override
+    public void inserir(Pessoa2 _pessoa){
+        pessoas[cursor] = _pessoa;
+        cursor++;
+    }
+
+    @Override
+    public int tamanhoColecaoNaoNull(){
+        for(int i=0; i<cursor;i++){
+            System.out.println(pessoas[i].getNome());
+        }
+        return 5-cursor;
+    }
+
+    public ColecaoVetor() {
+        this.cursor = 0;
+        this.pessoas = new Pessoa2[5];
+    }
+
+
+}
+
+class Pessoa2 {
+    protected String nome;
+    protected String cpf; 
+
+    public Pessoa2(String _nome, String _cpf){
+        nome = _nome;
+        cpf = _cpf;    
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }  
+}
+
+class Professor extends Pessoa2 {
+    protected String formacao;
+    protected String area;
+
+    public Professor(String _nome, String _cpf, String formacao, String area) {
+        super(_nome, _cpf);
+        this.formacao = formacao;
+        this.area = area;
+    }
+    
+}
+
+class Aluno extends Pessoa2 {
+    protected String matricula;
+    protected String curso;
+
+    public Aluno(String _nome, String _cpf, String matricula, String curso) {
+        super(_nome, _cpf);
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}
+
